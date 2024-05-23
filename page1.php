8720603f92a00f0044ecbeac040099ed45ff789e
Aldin285 <148047232+Aldin285@users.noreply.github.com>
2024-05-23 11:22:16 +0200

    suppression des bases inutiles+test

--- page1.php
+++ page1.php
@@ -1,47 +1,29 @@
 <!DOCTYPE html>
 <html>
-<head><title>Premier script php</title></head>
+<head><title>essaie 1</title></head>
 <body>
+
+<h1>fhouhfhfhs</h1>
 <?php
 try {
 
-    $service=$_POST["service"];
+    
 
-    echo" <h1>Liste de tous les employés du département:";
-    switch($service)
-    {
-        case 10: echo " recherche </h1> <br/>";
-        break;
-        case 20: echo " vente</h1> <br/>";
-        break;
-        case 30: echo " direction</h1> <br/>";
-        break;
-        case 40: echo " fabrication</h1> <br/>";
-        break;
-
-
-    };
-
-    $connexion= new PDO('mysql:host=localhost;dbname=departement','root','',);
-    $requete = 'SELECT e.nom as "employesNom",num,fonction,d.nom,lieu
-                FROM employes as e  join departements as d on
-                (d.dept=e.dept)
-                where d.dept='.$service;
+    $connexion= new PDO('mysql:host=localhost;dbname=locautoV2','root','',);
+    $requete = 'SELECT nom,prenom,libelle
+                FROM client  natural join type_de_client';
+                
     $resultat = $connexion->query($requete);
     echo "<table>\n";
-    echo "<tr><th>Nom de l'employe</th>
-            <th>Numero</th>
-            <th>Fonction</th>
-            <th>Nom du departement</th>
-            <th>Lieu</th>
+    echo "<tr><th>Nom client /th>
+            <th>prénom</th>
+            <th>type</th>
          </tr>";
     while ($ligne = $resultat->fetch()) {
             echo "\t<tr>\n";
-            echo "\t\t<td>" . $ligne["employesNom"] . "</td>\n";
-            echo "\t\t<td>" . $ligne["num"] . "</td>\n";
-            echo "\t\t<td>" . $ligne["fonction"] . "</td>\n";
-            echo "\t\t<td>" . $ligne["nom"] . "</td>\n";
-            echo "\t\t<td>" . $ligne["lieu"] . "</td>\n";
+            echo "\t\t<td>". $ligne["nom"] ."</a></td>\n";
+            echo "\t\t<td>" . $ligne["prenom"] . "</td>\n";
+            echo "\t\t<td>" . $ligne["libelle"] . "</td>\n";
             echo "\t</tr>\n";
     }
     echo "</table>";

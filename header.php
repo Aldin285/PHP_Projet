bdf662555bcd92cf32bac91ece1d0193bf84985d
Mohamed <mohamed.mbow@supdevinci-edu.fr>
2024-06-05 13:35:13 +0200

    quelque modif

---
+++ header.php
@@ -0,0 +1,22 @@
+<?php
+            echo " <header class='header'>
+
+            <div id='menu-btn' class='fas fa-bars'></div>
+
+            <a href='#' class='logo'> <span>Car</span>Go</a>
+
+            <nav class='navbar>
+                <a href='#home'>Home</a>
+                <a href='voiture_choix.php'>Vehicule</a>
+                <a href='#services'>Services</a>
+                <a href='#contact'>Contact</a>
+            </nav>
+
+            <div id='Login-btn'>
+                <button class='btn'>Profil</button>
+                <i class='far fa-user'></i>
+            </div>
+
+        </header>";
+       
+?>
--- voiture_choix.php
+++ voiture_choix.php
@@ -1,36 +1,54 @@
+<?php
+    session_start();
+?>
+
 <!DOCTYPE html>
-<html>
-    <head>
-        <title> page1 </title>
-    </head>
-        <body>
-        <form action="voiture_affichage.php" method="post">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CarGo</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+ 
+
+    <form action="voiture_affichage.php" method="post">
         
         <label for="modele-select">choix modele:</label>
+
         <select name="modele" size="1" id="modele-select">
-        <?php
-            try {
-                $connexion = new PDO('mysql:host=localhost;dbname=locautoV2',
-                'root', '');
-                $requete = 'SELECT * FROM modele';
-                $resultat = $connexion->query($requete);
-                while ($ligne = $resultat->fetch()) {
-                    echo "\t\t<option value ='" . $ligne["id_modele"] . "'>"
-                    . $ligne["libelle"] . "</option>\n";
+
+                <?php
+ 
+                    try {
+                        $connexion = new PDO('mysql:host=localhost;dbname=locautoV2',
+                        'root', '');
+                        $requete = 'SELECT * FROM modele';
+                        $resultat = $connexion->query($requete);
+                        while ($ligne = $resultat->fetch()) {
+                            echo "\t\t<option value ='" . $ligne["id_modele"] . "'>"
+                            . $ligne["libelle"] . "</option>\n";
+                            }
+                        }
+                        catch (PDOException $e) {
+                        echo "Erreur : " . $e->getMessage() . "<br/>";
+                        die();
                     }
-                }
-                catch (PDOException $e) {
-                echo "Erreur : " . $e->getMessage() . "<br/>";
-                die();
-            }
-        ?>
-        </select>
+
+                    echo "</select>";
+                    include_once("header.php")
+                    
+                ?>
+        
 
 
         <!-- Validation -->
         <br/><br/>
         <input type="submit" value="Valider"/>
         
-        </form>
-    </body>
+    </form>
+</body>
 </html>

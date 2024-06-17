dedeae68117921d51d2d27ab834664c92bafb738
Mohamed <mohamed.mbow@supdevinci-edu.fr>
2024-06-18 01:24:36 +0200

    integration de mon code dans le dossier pour permettre aux autres de voir et sauvegarder

--- connection/se_connecter.php
+++ connection/se_connecter.php
@@ -4,18 +4,40 @@ session_start();
 // connexion à un compte:
 <!DOCTYPE html>
 <html>
-    <head><title>Connexion compte</title></head>
+    <head>
+        <title>Connexion compte</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+        <link rel="stylesheet" href="style.css">
+    </head>
+
         <body>
-            <form action="se_connecter2.php" method=get>
-            <h2>CONNEXION</h2>
-            <label for="mail"><b>Adresse mail:</b></label>
-            <input type="text" name="mail" size="30" placeholder="Veuillez saisir votre adresse mail " required /><br/><br />
 
-            <label for="password"><b>Mot de passe:</b></label>
-            <input type="password" name="password" size="30" minlength='8'  placeholder="Veuillez saisir votre mot de passe " required /><br/><br />
 
-            <p><input type="submit" value = "Se connecter" />
-            <input type="reset" value="Annuler" /></p>
-            </form>
+            <div class="login-form-container">
+
+
+                <form method="POST" action="">
+
+                    <form action="se_connecter2.php" method=get>
+
+                    <h3>User login</h3>
+
+                    <input type="email" name="email" placeholder="email" class="box">
+                    <input type="password" name="password" size="30" minlength='8' placeholder="password" class="box" required>
+
+                    <p>Forget your password <a href="#"> click here</a></p>
+                    <input type="submit" value="Login now" class="btn">
+                    <p>or login with</p>
+
+                    <div class="buttons">
+                        <a href="#" class="btn">google</a>
+                        <a href="#" class="btn">facebook</a>
+                    </div>
+                    <p>Don't have an account? <a href="#">create one</a></p>
+    
+
+                </form>
+
+            </div>
         </body>
 </html>
